0d2a049b01539e319242ef91a3cd869607ac290e
Quentin Read <quentin.read@gmail.com>
2016-12-20 08:11:28 -0600

    Keep trying to merge the forestlight data

--- code/readshade.r
+++ code/readshade.r
@@ -39,3 +39,54 @@ lightdf <- read.csv(file.path(fp, 'Light.csv'), stringsAsFactors = FALSE)
 growthdf$Site <- gsub(' ', '_', growthdf$Site)
 
 mergeddf <- merge(growthdf, lightdf, by=c("Site", "Site_code", "Line", "Main_Stem", "LTER_Tag", "ENQ_Tag"), all.x=T, all.y=F)
+
+
+# Second attempt to merge data (16 Dec) -----------------------------------
+
+setwd('C:/Users/Q/Google Drive/ForestLight/data/Data To Merge/')
+bcidf <- read.csv('BCILight.csv', stringsAsFactors = FALSE)
+growthdf <- read.csv('BiomassAndGrowthSTM2014Grady!.csv', stringsAsFactors = FALSE)
+harvdf <- read.csv('HarvLight.csv', stringsAsFactors = FALSE)
+
+# Combine bci and harvard
+lightdf <- rbind(bcidf, harvdf[, names(bcidf)])
+
+# Add NA's to enqtag for lightdf
+lightdf$ENQ_Tag[lightdf$ENQ_Tag == ''] <- NA
+lightdf$Main_Stem <- as.numeric(lightdf$Main_Stem)
+lightdf$Site <- gsub(' ', '_', lightdf$Site)
+lightdfsub <- subset(lightdf, !is.na(CII))
+
+# Correct Site name on growthdf
+growthdf$Site <- gsub(' ', '_', growthdf$Site)
+
+growthdf$Main_Stem <- as.numeric(growthdf$Main_Stem)
+
+# Rename columns of growthdf to match those of lightdf
+names(growthdf)[names(growthdf) == 'LTER_Tag_.'] <- 'LTER_Tag'
+names(growthdf)[names(growthdf) == 'ENQ_Tag_.'] <- 'ENQ_Tag'
+names(growthdf)[names(growthdf) == 'Site_code'] <- 'Site_Code'
+
+growthdf$origidx <- 1:nrow(growthdf)
+
+mergeddf <- merge(growthdf, lightdfsub, by=c("Site", "Site_Code", "Line", "Main_Stem", "LTER_Tag", "ENQ_Tag"), all.x=T, all.y=F, sort = F)
+
+t1 <- table(mergeddf$origidx)
+problemrows <- as.numeric(names(t1[t1>1]))
+growthdf[problemrows,]
+mergeddf[mergeddf$origidx %in% problemrows, ]
+
+lightdf[which(lightdf$ENQ_Tag == '5186'),]
+
+# Remove problematic row from lightdf
+lightdf <- lightdf[-6846,]
+
+mergeddf <- merge(growthdf[,-88], lightdfsub, by=c("Site", "Site_Code", "Line", "Main_Stem", "LTER_Tag", "ENQ_Tag"), all.x=T, all.y=F, sort = F)
+
+# Sort merged df
+mergeddf <- mergeddf[with(mergeddf, order(Site, Site_Code, Line, LTER_Tag, ENQ_Tag)), ]
+
+write.csv(mergeddf, file = 'GrowthLightMerged.csv', row.names=FALSE)
+
+tagnames <- c('Main_Stem','LTER_Tag')
+mergeddf_nolter <- merge(growthdf[, !names(growthdf) %in% c('origidx',tagnames)], lightdfsub[, !names(lightdfsub) %in% tagnames], all.x=T, all.y=F, sort=F)
