628840749c44208cf98e900d2031184b227a2d15
Quentin Read <quentin.read@gmail.com>
2018-01-23 09:58:53 -0500

    Redo binning and plotting without shrubs

--- code/workflow22jan_noshrub.r
+++ code/workflow22jan_noshrub.r
@@ -1,206 +1,22 @@
-# Full data processing pipeline
-# Include new classification from Nadja
-# Later, include option to split by growth form or remove shrubs from the dataset.
-
-# Each bin dataframe should include the following
-# 1 all individuals
-# 2 all individuals classified in a FG
-# 3-7 FGs 1 through 5
-# 8 individuals not classified in a FG
-
-group_names <- c('all','all_classified','fg1','fg2','fg3','fg4','fg5','unclassified')
-
-load('C:/Users/Q/Dropbox/projects/forestlight/bcidata/bciqcrun.R')
-
-# Modification 25 August: get rid of young and edge trees for all datasets. This will reduce the number of hectares but will be most correct.
-
-for (i in 1:7) {
-  assign(paste0('bci.full', i), subset(get(paste0('bci.full', i)), !young & !edge))
-  assign(paste0('bci.stem', i), subset(get(paste0('bci.stem', i)), !young & !edge))
-}
-
-growth8590 <- subset(growth8590, !young & !edge)
-growth9095 <- subset(growth9095, !young & !edge)
-
-library(dplyr)
-
-# Load Nadja's data
-fgbci <- read.table('C:/Users/Q/google_drive/ForestLight/data/Ruger/functional_groups_BCI.txt', stringsAsFactors = FALSE)
-
-# 2. Convert all census data frames to the correct units (dbh in cm, not mm; agb in kg, not Mg)
-# Simultaneously, calculate biomass increments for each stem from the previous census to the current one. Combine everything into a single data frame if possible.
-# Also get rid of the young (secondary) forest patches.
-
-bci_production <- list()
-
-for (i in 2:7) {
-  dat_i <- get(paste0('bci.full',i))
-  dat_iminus1 <- get(paste0('bci.full',i-1))
-  bci_production[[i-1]] <- dat_i$agb_corr - dat_iminus1$agb_corr
-  dat_i$production <- dat_i$agb_corr - dat_iminus1$agb_corr
-  assign(paste0('bci.full', i), dat_i)
-}
-
-bci_production <- do.call(cbind, bci_production)
-bci_production <- as.data.frame(bci_production)
-names(bci_production) <- paste0('production', c('8085','8590','9095','9500','0005','0510'))
-
-# Amendment 11 Oct. : Add dbh increment, used to detect production outliers.
-
-for (i in 2:7) {
-  dat_i <- get(paste0('bci.full',i))
-  dat_iminus1 <- get(paste0('bci.full',i-1))
-  dbhlastcensus <- dat_iminus1$dbh_corr
-  dbhlastcensus[is.na(dbhlastcensus)] <- 0
-  dat_i$dbhinc <- dat_i$dbh_corr - dbhlastcensus
-  dat_i$dbhlastcensus <- dbhlastcensus
-  assign(paste0('bci.full', i), dat_i)
-}
-
-
-
-
-bcicensusdat <- list()
-
-for (i in 2:7) {
-  bcicensusdat[[i-1]] <- get(paste0('bci.full',i)) %>%
-    filter(DFstatus == 'alive') %>%
-    mutate(dbh_corr = dbh_corr/10,
-           agb_corr = agb_corr * 1000,
-           production = production * 1000,
-           dbhinc = dbhinc/10,
-           dbhlastcensus = dbhlastcensus/10) %>%
-    filter(!young)
-}
-
-####
-# Amendment 11 Oct.
-# Remove production outliers.
-# Criteria: 
-# 1. If the tree was not in the previous census and its current dbh is >10, remove.
-# 2. If the tree grew over 20 cm dbh between censuses, remove.
-
-bcicensusdat <- lapply(bcicensusdat, function(x) {
-  x$production[x$dbhinc > 20 | (x$dbhinc > 10 & x$dbhlastcensus == 0)] <- NA
-  x
-})
-
-
-# 3. Join with shade tolerance (Rüger) groups.
-
-fgbci_less <- fgbci %>%
-  select(sp, grform, fg) %>%
-  mutate(sp = tolower(sp))
-
-for (i in 1:6) {
-  bcicensusdat[[i]] <- bcicensusdat[[i]] %>%
-    left_join(fgbci_less)
-}
-
-# 4. Join the appropriate years with Nadja's light data.
-# bcicensusdat[[2]] is 1985-1990
-# bcicensusdat[[3]] is 1990-1995
-
-bcicensusdat[[2]] <- bcicensusdat[[2]] %>%
-  mutate(tag = as.numeric(tag)) %>%
-  left_join(growth8590 %>% select(tag, light, young, edge))
-
-bcicensusdat[[3]] <- bcicensusdat[[3]] %>%
-  mutate(tag = as.numeric(tag)) %>%
-  left_join(growth9095 %>% select(tag, light, young, edge))
-
-# 5. Run allometries to get total light received.
-
-# Function to get a rough approximation of insolation by latitude.
-
-insolation <- function(lat) {
-  lat <- lat * pi/180 # to radians
-  y <- sin(lat)
-  0.25 * 1367 * (1 - 0.482 * (3*y^2 - 1)/2)
-}
-
-# Insolation at BCI, 9.2 degrees N
-(insol_bci <- insolation(9.2))
-
-# Function to get tree height and crown dimensions from dbh
-# Use same parameters for all trees, taken from Bohlman and O'Brien
-
-tp <- function(dbh) {
-  h <- exp(.438 + .595 * log(dbh))    # Height
-  cd <- exp(-.157 + .702 * log(dbh))  # Crown depth
-  cr <- exp(-.438 + .658 * log(dbh))  # Crown radius
-  cV <- exp(-.681 + 2.02 * log(dbh))  # Crown volume
-  data.frame(h=h, cd=cd, cr=cr, cV=cV)
-}
-
-######
-# If it's needed to edit the allometries for different species groups, add it here.
-######
-
-for (i in 2:3) {
-  
-  crowndim <- tp(bcicensusdat[[i]]$dbh_corr) 
-  bcicensusdat[[i]]$crownarea <- pi * crowndim$cr^2
-  bcicensusdat[[i]] <- transform(bcicensusdat[[i]], light_received = light * crownarea * insol_bci)
-  
-}
-
-# Classification of light into 3 groups.
-# This is percent of full irradiance, not the total light received.
-
-for (i in 2:3) {
-  
-  light_groups <- cut(bcicensusdat[[i]]$light, breaks = 3)
-  table(light_groups)
-  light_groupcodes <- factor(light_groups, 
-                             labels = c('Low light','Intermediate light','High light'))
-  
-  bcicensusdat[[i]]$light_group <- light_groupcodes
-  
-}
-
-alltreedat <- list()
-fgdat <- replicate(6, list())
-
-for (i in 1:6) {
-  
-  alltreedat[[i]] <- subset(bcicensusdat[[i]], 
-                            !is.na(dbh_corr) & production > 0)
-}
-
-for (i in 1:6) {
-  for (j in 1:5) {
-    fgdat[[j]][[i]] <- subset(alltreedat[[i]], fg == j)
-  }
-  fgdat[[6]][[i]] <- subset(alltreedat[[i]], is.na(fg))
-}
-
-# for 2 and 3, make additional data frames that have only trees with light measurements.
-alltree_light_90 <- subset(alltreedat[[2]], !is.na(light))
-alltree_light_95 <- subset(alltreedat[[3]], !is.na(light))
-
-light_fg_90 <- lapply(fgdat, function(x) subset(x[[2]], !is.na(light)))
-light_fg_95 <- lapply(fgdat, function(x) subset(x[[3]], !is.na(light)))
-
-
-# Save raw data as an object
-save(alltreedat, fgdat, alltree_light_90, alltree_light_95, light_fg_90, light_fg_95, file = 'C:/Users/Q/google_drive/ForestLight/data/rawdataobj_22jan.r')
-
-####################################################################
-
-# Binning and error bars: all years combined ------------------------------
-
-# Script copied and modified on 22 Jan 2018. 
-# Procedure: use all 5 FGs (but not unclassified) together to get the bin edges
-# Then apply those bin edges to each FG in isolation.
-# That way each FG are given the same bin edges.
+# Workflow binning and plotting, with shrubs removed
+# 22 Jan
 
 load('C:/Users/Q/google_drive/ForestLight/data/rawdataobj_22jan.r')
 group_names <- c('all','all_classified','fg1','fg2','fg3','fg4','fg5','unclassified')
+
+get_rid_of_shrubs <- function(x) {
+  x[x$grform %in% c('M','T','U') || is.na(x$grform), ]
+}
+
+alltreedat <- lapply(alltreedat, get_rid_of_shrubs)
+fgdat <- lapply(fgdat, function(z) lapply(z, get_rid_of_shrubs))
+
 source('code/allfunctions27july.r')
 
 # Set number of bins       
 numbins <- 20
+
+
 
 # Make a version of alltreedat without the unclassified trees
 alltreedat_classified <- lapply(alltreedat, function(x) subset(x, !is.na(fg)))
@@ -210,7 +26,7 @@ allyeardbh_classified <- unlist(lapply(alltreedat_classified[2:6], '[', , 'dbh_c
 dbhbin_allclassified <- logbin(x = allyeardbh_classified, y = NULL, n = numbins)
 
 # Bin all trees including unclassified
-allyeardbh <- unlist(lapply(alltreedat[2:6], '[', , 'dbh_corr'))
+allyeardbh <- na.omit(unlist(lapply(alltreedat[2:6], '[', , 'dbh_corr')))
 dbhbin_all <- logbin(x = allyeardbh, y = NULL, n = numbins)
 
 allyeardbh_fg <- lapply(fgdat, function(x) unlist(lapply(x[2:6], '[', , 'dbh_corr')))
@@ -252,9 +68,9 @@ densitybin_5census <- cbind(fg = rep(group_names, each = numbins),
 # Take the mean and 2.5, 50 (median), 97.5 quantiles within each bin.
 # Do it across all years.
 
-allyearprod <- unlist(lapply(alltreedat[2:6], '[', , 'production'))
+allyearprod <- na.omit(unlist(lapply(alltreedat[2:6], '[', , 'production')))
 allyearprod_classified <- unlist(lapply(alltreedat_classified[2:6], '[', , 'production'))
-allyearprod_fg <- lapply(fgdat, function(x) unlist(lapply(x[2:6], '[', , 'production')))
+allyearprod_fg <- lapply(fgdat, function(x) na.omit(unlist(lapply(x[2:6], '[', , 'production'))))
 
 fakebin_across_years <- function(dat_values, dat_classes, edges, mean = 'geometric', n_census = 5) {
   qprobs <- c(0.025, 0.25, 0.5, 0.75, 0.975)
@@ -287,7 +103,7 @@ for (i in 1:6) {
 
 indivproductionbin_5census <- cbind(fg = rep(group_names, each = numbins),
                                     rbind(prodbin_all_5census, prodbin_allclassified_5census, do.call('rbind', prodbin_fg_5census)))
-  
+
 # Total production
 # Do the binning for each year separately, as for density, then find min, max, and median.
 
@@ -306,7 +122,7 @@ totalprodbin_fg_5census <- lapply(totalprodbin_fg_byyear, bin_across_years)
 
 # Combine into single df
 totalproductionbin_5census <- cbind(fg = rep(group_names, each = numbins), 
-                            rbind(totalprodbin_all_5census, totalprodbin_allclassified_5census, do.call('rbind', totalprodbin_fg_5census)))
+                                    rbind(totalprodbin_all_5census, totalprodbin_allclassified_5census, do.call('rbind', totalprodbin_fg_5census)))
 
 # Total light received and crown area
 # 1990 and 1995 only
@@ -326,7 +142,7 @@ crownareabin_allclassified_2census <- bin_across_years(crownareabin_allclassifie
 crownareabin_fg_2census <- lapply(crownareabin_fg_byyear, bin_across_years)
 
 crownareabin_2census <- cbind(fg = rep(group_names, each = numbins), 
-                                    rbind(crownareabin_all_2census, crownareabin_allclassified_2census, do.call('rbind', crownareabin_fg_2census)))
+                              rbind(crownareabin_all_2census, crownareabin_allclassified_2census, do.call('rbind', crownareabin_fg_2census)))
 
 ## light received
 lightreceivedbin_alltree_byyear <- lapply(alltreedat[2:3], function(z) with(subset(z, !is.na(light_received)), logbin_setedges(x = dbh_corr, y = light_received, edges = dbhbin_all)))
@@ -343,7 +159,7 @@ lightreceivedbin_allclassified_2census <- bin_across_years(lightreceivedbin_allc
 lightreceivedbin_fg_2census <- lapply(lightreceivedbin_fg_byyear, bin_across_years)
 
 lightreceivedbin_2census <- cbind(fg = rep(group_names, each = numbins), 
-                              rbind(lightreceivedbin_all_2census, lightreceivedbin_allclassified_2census, do.call('rbind', lightreceivedbin_fg_2census)))
+                                  rbind(lightreceivedbin_all_2census, lightreceivedbin_allclassified_2census, do.call('rbind', lightreceivedbin_fg_2census)))
 
 # Production and light received per meter squared of crown area.
 # Divide production by crown area and bin (1990 and 1995)
@@ -388,10 +204,10 @@ fpdata <- 'C:/Users/Q/google_drive/ForestLight/data/data_22jan2018'
 file_names <- c('densitybin_5census', 'indivproductionbin_5census', 'totalproductionbin_5census', 'crownareabin_2census', 'lightreceivedbin_2census', 'indivprodperareabin_2census')
 
 for (i in file_names) {
-  write.csv(get(i), file=file.path(fpdata, paste0(i,'.csv')), row.names = FALSE)
+  write.csv(get(i), file=file.path(fpdata, paste0('noshrub_',i,'.csv')), row.names = FALSE)
 }
 
-save(list = file_names, file = file.path(fpdata, 'bin_object.RData'))
+save(list = file_names, file = file.path(fpdata, 'noshrub_bin_object.RData'))
 
 # Load data and plot
 # 04 Oct 2017
@@ -403,7 +219,7 @@ save(list = file_names, file = file.path(fpdata, 'bin_object.RData'))
 file_names <- c('densitybin_5census', 'indivproductionbin_5census', 'totalproductionbin_5census', 'crownareabin_2census', 'lightreceivedbin_2census', 'indivprodperareabin_2census')
 
 for (i in file_names) {
-  assign(i, read.csv(file.path(fpdata, paste0(i,'.csv')), stringsAsFactors = FALSE))
+  assign(i, read.csv(file.path(fpdata, paste0('noshrub_',i,'.csv')), stringsAsFactors = FALSE))
 }
 
 # Plot data ---------------------------------------------------------------
@@ -480,18 +296,18 @@ fig_4c <- indivprodperareabin_2census %>%
   scale_color_manual(values = guild_colors) +
   panel_border(colour = 'black')
 
-pdf('C:/Users/Q/google_drive/ForestLight/figs/figures_22jan2018/figs3and4.pdf', height = 6, width = 6)
-  print(fig_3a + ggtitle('3A'))
-  print(fig_3b + ggtitle('3B'))
-  print(fig_3c + ggtitle('3C'))
-  print(fig_4a + ggtitle('4A'))
-  print(fig_4b + ggtitle('4B'))
-  print(fig_4c + ggtitle('4C'))
+pdf('C:/Users/Q/google_drive/ForestLight/figs/figures_22jan2018/noshrub_figs3and4.pdf', height = 6, width = 6)
+print(fig_3a + ggtitle('3A'))
+print(fig_3b + ggtitle('3B'))
+print(fig_3c + ggtitle('3C'))
+print(fig_4a + ggtitle('4A'))
+print(fig_4b + ggtitle('4B'))
+print(fig_4c + ggtitle('4C'))
 dev.off()
 
 # Make a figure to show the FGs
-ggplot(fgbci, aes(x = X1, y = X2, color = factor(fg))) +
+ggplot(subset(fgbci, grform %in% c('M','T','U') || is.na(grform)), aes(x = X1, y = X2, color = factor(fg))) +
   geom_point() +
   labs(x = 'X1 performance/recruitment', y = 'X2 shade/gap') +
   scale_color_manual(values = guild_colors)
-ggsave('C:/Users/Q/google_drive/ForestLight/figs/figures_22jan2018/funcgroups.pdf', height=6, width=6)
+ggsave('C:/Users/Q/google_drive/ForestLight/figs/figures_22jan2018/noshrub_funcgroups.pdf', height=6, width=6)
