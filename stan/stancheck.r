e89a28fe2b37223308e95ae6875285f5d53dc934
Quentin Read <quentin.read@gmail.com>
2018-03-09 10:56:19 -0500

    check which rstan jobs are completed

---
+++ stan/stancheck.r
@@ -0,0 +1,9 @@
+
+z <- expand.grid(year = c(1990, 1995, 2000, 2005, 2010),
+				 fg = c('fg1', 'fg2', 'fg3', 'fg4', 'fg5', 'unclassified', 'alltree'),
+				 model_name = c('ppow', 'wpow', 'pexp', 'wexp'),
+				 stringsAsFactors = FALSE)
+
+i <- 1:nrow(z)
+todo <- which(!file.exists(file.path('~/forestlight/stanoutput', paste0('fit_', z$model_name[i], '_', z$fg[i], '_', z$year[i], '.r'))))
+paste(todo, collapse = ',')
