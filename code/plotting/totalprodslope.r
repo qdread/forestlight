06e7522ae602f0a625c6204f1be0293ad57388b7
Quentin Read <quentin.read@gmail.com>
2018-04-30 09:21:55 -0400

    Figure showing slopes of energy equivalence using Weibull
    Also plot slopes for dens and indiv prod

---
+++ code/plotting/totalprodslope.r
@@ -0,0 +1,60 @@
+
+fp <- 'C:/Users/Q/google_drive/ForestLight/data/data_forplotting_12apr2018' ## CHANGE PATH AS NEEDED
+
+# Read all the csvs in directory.
+for (i in dir(fp, pattern = '.csv')) {
+  n <- gsub('.csv','',i)
+  assign(n, read.csv(file.path(fp, i), stringsAsFactors = FALSE))
+}
+
+# Plot total production slope
+
+library(dplyr)
+library(cowplot)
+
+# Use formula d log y / d log x = x/y dy/dx to get slope of total production.
+slope_totalprod <- pred_totalprod %>%
+  filter(year == 1995, !fg %in% 'unclassified', dens_model == 'weibull', prod_model == 'powerlawexp') %>%
+  group_by(fg) %>%
+  mutate_at(vars(starts_with('q')), funs(c(NA, (dbh[-1]/.[-1]) * diff(.)/diff(dbh))))
+
+colors <- c('black', RColorBrewer::brewer.pal(5, 'Set1'))
+
+ggplot(slope_totalprod, aes(x=dbh, y=q50, ymin=q025, ymax=q975, group=fg, color=fg, fill=fg)) +
+  geom_ribbon(alpha = 0.5) +
+  geom_line() +
+  scale_x_log10() + scale_y_continuous(limits = c(-10, 4)) +
+  geom_hline(yintercept = 0, linetype = 'dashed') +
+  scale_color_manual(values=colors) + scale_fill_manual(values=colors)
+  
+
+# Show intermediate only
+ggplot(slope_totalprod, aes(x=dbh, y=q50, ymin=q025, ymax=q975, group=fg, color=fg, fill=fg)) +
+  geom_ribbon(alpha = 0.5) +
+  geom_line() +
+  scale_x_log10(limits = c(5,50), expand=c(0,0), breaks = c(5,10,20,50), name = 'Diameter (cm)') + 
+  scale_y_continuous(limits = c(-10, 2), name = 'Total growth slope') +
+  geom_hline(yintercept = 0, linetype = 'dashed') +
+  scale_color_manual(values=colors) + scale_fill_manual(values=colors)
+
+# Slopes of density and growth too ----------------------------------------
+
+slope_density <- pred_dens %>%
+  filter(year == 1995, !fg %in% 'unclassified', dens_model == 'weibull', prod_model == 'powerlawexp') %>%
+  group_by(fg) %>%
+  mutate_at(vars(starts_with('q')), funs(c(NA, (dbh[-1]/.[-1]) * diff(.)/diff(dbh))))
+
+slope_indivprod <- pred_indivprod %>%
+  filter(year == 1995, !fg %in% 'unclassified', dens_model == 'weibull', prod_model == 'powerlawexp') %>%
+  group_by(fg) %>%
+  mutate_at(vars(starts_with('q')), funs(c(NA, (dbh[-1]/.[-1]) * diff(.)/diff(dbh))))
+
+ggplot(slope_totalprod, aes(x=dbh, y=q50, ymin=q025, ymax=q975, group=fg, color=fg, fill=fg)) +
+  geom_line(size = 1.5) +
+  geom_line(data = slope_indivprod) +
+  geom_line(data = slope_density) +
+  scale_x_log10(limits = c(5,50), expand=c(0,0), breaks = c(5,10,20,50), name = 'Diameter (cm)') + 
+  scale_y_continuous(limits = c(-10, 4), name = 'Slope', expand = c(0,0)) +
+  geom_hline(yintercept = c(-2, 0, 2), linetype = 'dashed') +
+  scale_color_manual(values=colors) + scale_fill_manual(values=colors)
+
