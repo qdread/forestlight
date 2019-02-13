23e023aaeaf52e3362cd0256cebad09328449a83
Quentin Read <quentin.read@gmail.com>
2019-02-13 17:16:35 -0500

    Correct bugs in Stan shell scripts so new models will run Convert DOS files to UNIX where needed so that they will run on the cluster

--- stan/piecewise_workflow/makestanpiecewise.sh
+++ stan/piecewise_workflow/makestanpiecewise.sh
@@ -1,5 +1,5 @@
 cd ~/cmdstan-2.15.0
-module load GNU/6.2
+# module load GNU/6.2 # no longer needed with new operating system
 make build
 make O=3 ~/forestlight/stancode/model_d1p1
 make O=3 ~/forestlight/stancode/model_d1p2
@@ -7,3 +7,5 @@ make O=3 ~/forestlight/stancode/model_d2p1
 make O=3 ~/forestlight/stancode/model_d2p2
 make O=3 ~/forestlight/stancode/model_d3p1
 make O=3 ~/forestlight/stancode/model_d3p2
+make O=3 ~/forestlight/stancode/model_dwp1
+make O=3 ~/forestlight/stancode/model_dwp2
