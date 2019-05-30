bd7f4df02d4cfeb5462369676da45470ff6316bc
Quentin Read <quentin.read@gmail.com>
2019-05-30 13:33:10 -0400

    Debug model output extraction function And create function to write csvs from the output

--- stan/piecewise_workflow/extract_ci_piecewisefits_separate.r
+++ stan/piecewise_workflow/extract_ci_piecewisefits_separate.r
@@ -12,6 +12,8 @@ source('~/forestlight/stancode/extraction_functions_piecewise_separate.r')
 
 library(purrr)
 library(dplyr)
+library(foreach)
+library(doParallel)
 
 dens_df <- expand.grid(variable = 'density',
 					   dens_model = 1:3,
@@ -48,32 +50,39 @@ mod_df <- mod_df %>%
 dbh_pred <- exp(seq(log(1.2), log(315), length.out = 101))
 
 
-if (mod_df$variable[i] == 'density') {
-	fit_info <- extract_density(dens_model = mod_df$dens_model[i],
-								fg = mod_df$fg[i],
-								year = mod_df$year[i],
-								xmin = mod_df$xmin[i],
-								n = mod_df$n[i],
-								use_subset = FALSE)
-}
-if (mod_df$variable[i] == 'production') {
-	fit_info <- extract_production(prod_model = mod_df$prod_model[i],
-								fg = mod_df$fg[i],
-								year = mod_df$year[i],
-								xmin = mod_df$xmin[i],
-								n = mod_df$n[i],
-								total_production = mod_df$total_production[i],
-								use_subset = FALSE)
-}
-if (mod_df$variable[i] == 'total_production') {
-	fit_info <- extract_totalproduction(dens_model = mod_df$dens_model[i],
-								prod_model = mod_df$prod_model[i],
-								fg = mod_df$fg[i],
-								year = mod_df$year[i],
-								xmin = mod_df$xmin[i],
-								n = mod_df$n[i],
-								total_production = mod_df$total_production[i],
-								use_subset = FALSE)
-}
+registerDoParallel(cores = 4)
 
-save(fit_info, file = paste0('~/forestlight/stanoutput/fitinfo/pw_info_'mod_df$variable[i],'_',i,'.r'))
+tmp <- foreach(i = 1:nrow(mod_df)) %dopar% {
+
+	if (mod_df$variable[i] == 'density') {
+		fit_info <- extract_density(dens_model = mod_df$dens_model[i],
+									fg = mod_df$fg[i],
+									year = mod_df$year[i],
+									xmin = mod_df$xmin[i],
+									n = mod_df$n[i],
+									use_subset = FALSE)
+	}
+	if (mod_df$variable[i] == 'production') {
+		fit_info <- extract_production(prod_model = mod_df$prod_model[i],
+									fg = mod_df$fg[i],
+									year = mod_df$year[i],
+									xmin = mod_df$xmin[i],
+									n = mod_df$n[i],
+									total_production = mod_df$total_production[i],
+									use_subset = FALSE)
+	}
+	if (mod_df$variable[i] == 'total_production') {
+		fit_info <- extract_totalproduction(dens_model = mod_df$dens_model[i],
+									prod_model = mod_df$prod_model[i],
+									fg = mod_df$fg[i],
+									year = mod_df$year[i],
+									xmin = mod_df$xmin[i],
+									n = mod_df$n[i],
+									total_production = mod_df$total_production[i],
+									use_subset = FALSE)
+	}
+
+	save(fit_info, file = paste0('~/forestlight/stanoutput/fitinfo/pw_info_',mod_df$variable[i],'_',i,'.r'))
+	message('Fit ', i, ' saved')
+
+}
