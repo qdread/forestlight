973ac86c1859e618f6814547529f8d5d4746e4b1
John Grady <jgradym@gmail.com>
2019-06-25 17:05:03 -0500

    Update unified_plotting_code.R
    added fig 5

--- code/plotting/unified_plotting_code.R
+++ code/plotting/unified_plotting_code.R
@@ -35,6 +35,14 @@ theme_plant <- theme(panel.grid = element_blank(), #for Total Production
                       rect = element_rect(fill = "transparent"),
                       text = element_text(family = 'Helvetica')) 
 
+theme_facet <- theme(strip.background = element_rect(fill=NA),
+      panel.border = element_rect(color = "black", fill=NA,  size=.75),legend.position = 'none',
+      panel.background = element_blank(),
+      strip.text.x = element_blank(),
+      axis.text = element_text(size = 15, color = "black"), 
+      axis.ticks.length=unit(0.2,"cm"),
+      axis.title = element_text(size = 15)) 
+
 #---------------------------------------------------------------------------------------------
 # Fig 1: hand drawn schematics 
 #---------------------------------------------------------------------------------------------
@@ -470,8 +478,209 @@ p <- p + scale_y_continuous(position = "left", trans = "log10", breaks = c(10, 1
 p
 
 
-# ------------------------------- Fig 6 Relative Abundance  ------------------------------------
 
+################################################################################################
+# ------------------------------- Fig 5 Light Interception  ------------------------------------
+################################################################################################
+
+
+alltree_light_95 <- alltree_light_95 %>%
+  mutate(fg = if_else(!is.na(fg), paste0('fg',fg), as.character(NA)))
+
+dbhbin1995 <- with(alltree_light_95, logbin(x = dbh_corr, n = 20))
+
+lightperareafakebin_fg <- alltree_light_95 %>%
+  mutate(dbh_bin = cut(dbh_corr, breaks = c(dbhbin1995$bin_min[1], dbhbin1995$bin_max), labels = dbhbin1995$bin_midpoint, include.lowest = TRUE)) %>%
+  group_by(fg, dbh_bin) %>%
+  do(quantile(.$light_received/.$crownarea, c(0.025, 0.25, 0.5, 0.75, 0.975)) %>%
+       t %>% 
+       as.data.frame %>%
+       setNames(c('q025','q25','q50','q75','q975')))
+
+lightperareafakebin_all <- alltree_light_95 %>%
+  mutate(dbh_bin = cut(dbh_corr, breaks = c(dbhbin1995$bin_min[1], dbhbin1995$bin_max), labels = dbhbin1995$bin_midpoint, include.lowest = TRUE)) %>%
+  group_by(dbh_bin) %>%
+  do(quantile(.$light_received/.$crownarea, c(0.025, 0.25, 0.5, 0.75, 0.975)) %>%
+       t %>% 
+       as.data.frame %>%
+       setNames(c('q025','q25','q50','q75','q975')))
+
+lightperareafakebin_fg <- data.frame(fg = 'all', lightperareafakebin_all, stringsAsFactors = FALSE) %>%
+  rbind(as.data.frame(lightperareafakebin_fg)) %>%
+  mutate(dbh_bin = as.numeric(as.character(dbh_bin)))
+
+lightpervolfakebin_fg <- alltree_light_95 %>%
+  mutate(dbh_bin = cut(dbh_corr, breaks = c(dbhbin1995$bin_min[1], dbhbin1995$bin_max), labels = dbhbin1995$bin_midpoint, include.lowest = TRUE)) %>%
+  group_by(fg, dbh_bin) %>%
+  do(quantile(.$light_received/.$crownvolume, c(0.025, 0.25, 0.5, 0.75, 0.975)) %>%
+       t %>% 
+       as.data.frame %>%
+       setNames(c('q025','q25','q50','q75','q975')))
+
+lightpervolfakebin_all <- alltree_light_95 %>%
+  mutate(dbh_bin = cut(dbh_corr, breaks = c(dbhbin1995$bin_min[1], dbhbin1995$bin_max), labels = dbhbin1995$bin_midpoint, include.lowest = TRUE)) %>%
+  group_by(dbh_bin) %>%
+  do(quantile(.$light_received/.$crownvolume, c(0.025, 0.25, 0.5, 0.75, 0.975)) %>%
+       t %>% 
+       as.data.frame %>%
+       setNames(c('q025','q25','q50','q75','q975')))
+
+lightpervolfakebin_fg <- data.frame(fg = 'all', lightpervolfakebin_all, stringsAsFactors = FALSE) %>%
+  rbind(as.data.frame(lightpervolfakebin_fg)) %>%
+  mutate(dbh_bin = as.numeric(as.character(dbh_bin)))
+
+# Plot: raw data ----------------------------------------------------------
+
+exl <- expression(paste('Light per Crown Area (W cm'^-1, 'm'^-2, ')', sep = ''))
+exv <- expression(paste('Light per Crown Volume (W cm'^-1, ')', sep = ''))
+exd <- 'Diameter (cm)'
+
+####### by area ######
+# Each group
+ggplot() +
+  geom_point(alpha = 0.05, data = alltree_light_95 %>% filter(!is.na(fg)), aes(x = dbh_corr, y = light_received/crownarea), color = 'forestgreen') +
+  geom_pointrange(data = lightperareafakebin_fg %>% filter(!fg %in% 'all', !is.na(fg)), aes(x = dbh_bin, y = q50, ymin = q25, ymax = q75)) +
+  facet_wrap(~ fg) +
+  scale_x_log10(name = exd) +
+  scale_y_log10(name = exl) +
+  theme_facet
+
+# All together
+ggplot() +
+  geom_point(alpha = 0.05, data = alltree_light_95, aes(x = dbh_corr, y = light_received/crownarea), color = 'forestgreen') +
+  geom_pointrange(data = lightperareafakebin_fg %>% filter(fg %in% 'all'), aes(x = dbh_bin, y = q50, ymin = q25, ymax = q75)) +
+  scale_x_log10(name = exd) +
+  scale_y_log10(name = exl) +
+  theme_bw() 
+
+####### by vol #######
+# Each group
+ggplot() +
+  geom_point(alpha = 0.05, data = alltree_light_95 %>% filter(!is.na(fg)), aes(x = dbh_corr, y = light_received/crownvolume), color = 'forestgreen') +
+  geom_pointrange(data = lightpervolfakebin_fg %>% filter(!fg %in% 'all', !is.na(fg)), aes(x = dbh_bin, y = q50, ymin = q25, ymax = q75)) +
+  facet_wrap(~ fg) +
+  scale_x_log10(name = exd) +
+  scale_y_log10(name = exv) +
+  theme_bw() 
+
+# All together
+ggplot() +
+  geom_point(alpha = 0.05, data = alltree_light_95, aes(x = dbh_corr, y = light_received/crownvolume), color = 'forestgreen') +
+  geom_pointrange(data = lightpervolfakebin_fg %>% filter(fg %in% 'all'), aes(x = dbh_bin, y = q50, ymin = q25, ymax = q75)) +
+  scale_x_log10(name = exd) +
+  scale_y_log10(name = exv) +
+  theme_bw() 
+
+
+# Plot: hexagon plot ------------------------------------------------------
+
+alpha_value <- 0.6
+hexfill <- scale_fill_gradient(low = 'forestgreen', high = 'navy', trans = 'log', breaks = c(1,3,10,30,100,300))
+
+####### by area #######
+# Each group
+ggplot() +
+  geom_hex(alpha = alpha_value, data = alltree_light_95 %>% filter(!is.na(fg)), aes(x = dbh_corr, y = light_received/crownarea)) +
+  geom_pointrange(data = lightperareafakebin_fg %>% filter(!fg %in% 'all', !is.na(fg)), aes(x = dbh_bin, y = q50, ymin = q25, ymax = q75)) +
+  facet_wrap(~ fg) +
+  scale_x_log10(name = exd) +
+  scale_y_log10(name = exl) +
+  theme_facet() +
+  hexfill +
+  guides(fill = guide_legend(override.aes = list(alpha = alpha_value)))
+
+# All together
+ggplot() +
+  geom_hex(alpha = alpha_value, data = alltree_light_95, aes(x = dbh_corr, y = light_received/crownarea)) +
+  geom_pointrange(data = lightperareafakebin_fg %>% filter(fg %in% 'all'), aes(x = dbh_bin, y = q50, ymin = q25, ymax = q75)) +
+  scale_x_log10(name = exd) +
+  scale_y_log10(name = exl) +
+  theme_bw() +
+  hexfill +
+  guides(fill = guide_legend(override.aes = list(alpha = alpha_value)))
+
+####### by vol #######
+# Each group
+ggplot() +
+  geom_hex(alpha = alpha_value, data = alltree_light_95 %>% filter(!is.na(fg)), aes(x = dbh_corr, y = light_received/crownvolume)) +
+  geom_pointrange(data = lightpervolfakebin_fg %>% filter(!fg %in% 'all', !is.na(fg)), aes(x = dbh_bin, y = q50, ymin = q25, ymax = q75)) +
+  facet_wrap(~ fg) +
+  scale_x_log10(name = exd) +
+  scale_y_log10(name = exv) +
+  theme_bw() +
+  hexfill +
+  guides(fill = guide_legend(override.aes = list(alpha = alpha_value)))
+
+# All together
+ggplot() +
+  geom_hex(alpha = alpha_value, data = alltree_light_95, aes(x = dbh_corr, y = light_received/crownvolume)) +
+  geom_pointrange(data = lightpervolfakebin_fg %>% filter(fg %in% 'all'), aes(x = dbh_bin, y = q50, ymin = q25, ymax = q75)) +
+  scale_x_log10(name = exd) +
+  scale_y_log10(name = exv) +
+  theme_bw() +
+  hexfill +
+  guides(fill = guide_legend(override.aes = list(alpha = alpha_value)))
+
+# Plot: total unscaled light energy by dbh --------------------------------
+
+
+unscaledlightbydbhfakebin_fg <- alltree_light_95 %>%
+  mutate(dbh_bin = cut(dbh_corr, breaks = c(dbhbin1995$bin_min[1], dbhbin1995$bin_max), labels = dbhbin1995$bin_midpoint, include.lowest = TRUE)) %>%
+  group_by(fg, dbh_bin) %>%
+  do(quantile(.$light_received, c(0.025, 0.25, 0.5, 0.75, 0.975)) %>%
+       t %>% 
+       as.data.frame %>%
+       setNames(c('q025','q25','q50','q75','q975')))
+
+unscaledlightbydbhfakebin_all <- alltree_light_95 %>%
+  mutate(dbh_bin = cut(dbh_corr, breaks = c(dbhbin1995$bin_min[1], dbhbin1995$bin_max), labels = dbhbin1995$bin_midpoint, include.lowest = TRUE)) %>%
+  group_by(dbh_bin) %>%
+  do(quantile(.$light_received, c(0.025, 0.25, 0.5, 0.75, 0.975)) %>%
+       t %>% 
+       as.data.frame %>%
+       setNames(c('q025','q25','q50','q75','q975')))
+
+unscaledlightbydbhfakebin_fg <- data.frame(fg = 'all', unscaledlightbydbhfakebin_all, stringsAsFactors = FALSE) %>%
+  rbind(as.data.frame(unscaledlightbydbhfakebin_fg)) %>%
+  mutate(dbh_bin = as.numeric(as.character(dbh_bin)))
+
+alpha_value <- 0.6
+hexfill2 <- scale_fill_gradient(low = 'forestgreen', high = 'navy', trans = 'log', breaks = c(1,3,10,30,100,300))
+exl <- 'Light received (W)'
+exd <- 'Diameter (cm)'
+
+# Each group
+ggplot() +
+  geom_hex(alpha = alpha_value, data = alltree_light_95 %>% filter(!is.na(fg)), aes(x = dbh_corr, y = light_received)) +
+  geom_pointrange(data = unscaledlightbydbhfakebin_fg %>% filter(!fg %in% 'all', !is.na(fg)), aes(x = dbh_bin, y = q50, ymin = q25, ymax = q75)) +
+  facet_wrap(~ fg) +
+  scale_x_log10(name = exd) +
+  scale_y_log10(name = exl) +
+  theme_bw() +
+  hexfill2 +
+  guides(fill = guide_legend(override.aes = list(alpha = alpha_value)))
+
+# All together
+ggplot() +
+  geom_hex(alpha = alpha_value, data = alltree_light_95, aes(x = dbh_corr, y = light_received)) +
+  geom_pointrange(data = unscaledlightbydbhfakebin_fg %>% filter(fg %in% 'all'), aes(x = dbh_bin, y = q50, ymin = q25, ymax = q75)) +
+  scale_x_log10(name = exd) +
+  scale_y_log10(name = exl) +
+  theme_bw() +
+  hexfill2 +
+  guides(fill = guide_legend(override.aes = list(alpha = alpha_value)))
+
+# Very simple model
+loglogregressions <- alltree_light_95 %>%
+  group_by(fg) %>%
+  do(model = lm(log10(light_received) ~ log10(dbh_corr), data = .))
+
+lapply(loglogregressions$model, summary)
+
+
+################################################################################################
+# ------------------------------- Fig 6 Relative Abundance  ------------------------------------
+################################################################################################
 
 ###### Life history Relative Abundance by Size 
 
@@ -1013,7 +1222,7 @@ plot_prod <- function(year_to_plot = 1990,
 #hex_scale_3 <- scale_fill_gradientn(colours = rev(colorRampPalette(RColorBrewer::brewer.pal(9,'RdYlBu'), bias=0.3)(50)), guide = FALSE)
 
 # Biased and customized color scale
-#hex_scale_4 <- scale_fill_gradientn(colours = colorRampPalette(c('skyblue', 'goldenrod', 'indianred'), bias = 3)(50), guide = FALSE)
+#hex_scale_4 <- scale_fill_gradientn(colours = colorRampPalette(c('forestgreen', 'goldenrod', 'indianred'), bias = 3)(50), guide = FALSE)
 
 # Edit the hex scale argument to draw this with other color scales.
 #hex_scale_log <- scale_fill_gradientn(colours = colorRampPalette(gray.colors(9, start=.9, end=.1), bias=1)(50), trans = 'log', name = 'Number of\nindividuals', breaks = c(1,10,100,1000), labels = c(1,10,100,1000))
@@ -1308,7 +1517,7 @@ p_mean_1panel
 
 
 # 8. Raw data plot converted to hexbin instead of plain scatter
-#col1 <-colorRampPalette(c('skyblue', 'goldenrod', 'indianred'))
+#col1 <-colorRampPalette(c('forestgreen', 'goldenrod', 'indianred'))
 
 #hex_scale_log <- scale_fill_gradientn(colours = colorRampPalette(gray.colors(9, start=.9, end=.1), bias=3)(50),
 #                                  trans = 'log', name = 'Number of\nindividuals', breaks = c(1,10,100,1000), labels = c(1,10,100,1000))
@@ -1316,9 +1525,9 @@ p_mean_1panel
 #hex_scale_log <- scale_fill_gradientn(colours = colorRampPalette(c('khaki1', 'gold', 'red3'), bias=3)(50),
 #                                  trans = 'log', name = 'Number of\nindividuals', breaks = c(1,10,100,1000), labels = c(1,10,100,1000))
 
-#hex_scale_log <- scale_fill_gradientn(colours = colorRampPalette(c('aliceblue', 'skyblue','khaki1','red3'), bias=3)(50),
+#hex_scale_log <- scale_fill_gradientn(colours = colorRampPalette(c('aliceblue', 'forestgreen','khaki1','red3'), bias=3)(50),
 #                        trans = 'log', name = 'Number of\nindividuals', breaks = c(1,10,100,1000), labels = c(1,10,100,1000))
-#hex_scale_log <- scale_fill_gradientn(colours = colorRampPalette(c('skyblue', 'red3'), bias=3)(50),
+#hex_scale_log <- scale_fill_gradientn(colours = colorRampPalette(c('forestgreen', 'red3'), bias=3)(50),
 #                        trans = 'log', name = 'Number of\nindividuals', breaks = c(1,10,100,1000), labels = c(1,10,100,1000))
 #hex_scale_3b <- scale_fill_gradientn(colours = rev(colorRampPalette(RColorBrewer::brewer.pal(9,'RdYlBu'), bias=0.1)(50)), guide = F)
 hex_scale_log_colors <- scale_fill_gradientn(colours = colorRampPalette(rev(RColorBrewer::brewer.pal(9, 'RdYlBu')), bias=1)(50),
@@ -1683,7 +1892,7 @@ dev.off()
 # Plot: hexagon plot ------------------------------------------------------
 
 alpha_value <- 0.6
-hexfill <- scale_fill_gradient(low = 'skyblue', high = 'navy', trans = 'log', breaks = c(1,3,10,30,100,300))
+hexfill <- scale_fill_gradient(low = 'forestgreen', high = 'navy', trans = 'log', breaks = c(1,3,10,30,100,300))
 
 ####### by area #######
 # Each group
@@ -1753,7 +1962,7 @@ unscaledlightbydbhfakebin_fg <- data.frame(fg = 'all', unscaledlightbydbhfakebin
   mutate(dbh_bin = as.numeric(as.character(dbh_bin)))
 
 alpha_value <- 0.6
-hexfill2 <- scale_fill_gradient(low = 'skyblue', high = 'navy', trans = 'log', breaks = c(1,3,10,30,100,300))
+hexfill2 <- scale_fill_gradient(low = 'forestgreen', high = 'navy', trans = 'log', breaks = c(1,3,10,30,100,300))
 exl <- 'Light received (W)'
 exd <- 'Diameter (cm)'
 
