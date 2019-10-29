06e3edf046d57edacf9ed2b61213bc83bbb82f7d
Quentin Read <quentin.read@gmail.com>
2019-10-29 09:56:11 -0400

    Debug create_rdumps.r

--- stan/clean_workflow/create_rdumps.r
+++ stan/clean_workflow/create_rdumps.r
@@ -25,22 +25,19 @@ library(rstan)
 # Define function to create rdumps #
 # -------------------------------- #
 
-create_rdump <- function(dat, xvar, yvar, file_name = NULL, subsample = NULL, random_seed = NULL) {
+create_rdump <- function(dat, xvar, yvar, file_name = NULL, subsample = NULL, random_seed = NULL, x_range = c(1, 286)) {
   require(rstan)
   
   # Subset of data if specified
   if (!is.null(subsample) && nrow(dat) > subsample) {
-	if !is.null(random_seed) set.seed(random_seed)
+	  if (!is.null(random_seed)) set.seed(random_seed)
     dat <- dat[sample(nrow(dat), subsample, replace = FALSE), ]
   }
   
-  # Minimum and maximum dbh of BCI trees
-  ll <- 1
-  ul <- 316
-
+  # Create list. Min and max x are taken from data, lower and upper limits are specified manually (the latter is a legacy from the Weibull fits)
   x <- dat[, xvar]
   y <- dat[, yvar]
-  xdat <- list(N = length(x), x = x, y = y, x_min = min(x), x_max = max(x), LL = ll, UL = ul)
+  xdat <- list(N = length(x), x = x, y = y, x_min = min(x), x_max = max(x), LL = x_range[1], UL = x_range[2])
   
   if (!is.null(file_name)) {
     with(xdat, stan_rdump(names(xdat), file = file_name))
@@ -60,12 +57,12 @@ fgs <- c('fg1','fg2','fg3','fg4','fg5','unclassified')
 
 create_rdump(alltreedat[[3]], 'dbh_corr', 'production', file_name = file.path(fpdump, 'dump_alltree_1995.r'))
 
-imap(fgs, ~ create_rdump(fgdat[[.y]][[3]], 'dbh_corr' , 'production', file_name = file.path(fpdump, paste0('dump_', .x, '_1995.r'))))
+iwalk(fgs, ~ create_rdump(fgdat[[.y]][[3]], 'dbh_corr' , 'production', file_name = file.path(fpdump, paste0('dump_', .x, '_1995.r'))))
 
 ### diameter growth ~ diameter scalings
 create_rdump(alltreedat[[3]], 'dbh_corr', 'diam_growth_rate', file_name = file.path(fpdump, 'dump_diamgrowthscaling_alltree_1995.r'))
 
-imap(fgs, ~ create_rdump(fgdat[[.y]][[3]], 'dbh_corr' , 'diam_growth_rate', file_name = file.path(fpdump, paste0('dump_diamgrowthscaling_', .x, '_1995.r'))))
+iwalk(fgs, ~ create_rdump(fgdat[[.y]][[3]], 'dbh_corr' , 'diam_growth_rate', file_name = file.path(fpdump, paste0('dump_diamgrowthscaling_', .x, '_1995.r'))))
 
 ### light per area ~ growth per area scalings
 
@@ -78,21 +75,21 @@ create_rdump(dat95, 'light_area', 'production_area', file_name = file.path(fpdum
 
 dat95 %>%
 	group_by(fg) %>%
-	group_walk(~ create_rdump(., 'light_area', 'production_area', file_name = file.path(fpdump, paste0('dump_light_', .y, '_1995.r'))))
+	group_walk(~ create_rdump(as.data.frame(.), 'light_area', 'production_area', file_name = file.path(fpdump, paste0('dump_light_', .y, '_1995.r'))))
 	
 ### individual incoming light ~ diameter scalings
 create_rdump(dat95, 'light_received', 'dbh_corr', file_name = file.path(fpdump, 'dump_rawlightscaling_alltree_1995.r'))
 
 dat95 %>%
 	group_by(fg) %>%
-	group_walk(~ create_rdump(., 'light_received', 'dbh_corr', file_name = file.path(fpdump, paste0('dump_rawlightscaling_', .y, '_1995.r'))))
+	group_walk(~ create_rdump(as.data.frame(.), 'light_received', 'dbh_corr', file_name = file.path(fpdump, paste0('dump_rawlightscaling_', .y, '_1995.r'))))
 
 ### crown volume ~ diameter scalings
 create_rdump(dat95, 'crownvolume', 'dbh_corr', file_name = file.path(fpdump, 'dump_volumescaling_alltree_1995.r'))
 
 dat95 %>%
 	group_by(fg) %>%
-	group_walk(~ create_rdump(., 'crownvolume', 'dbh_corr', file_name = file.path(fpdump, paste0('dump_volumescaling_', .y, '_1995.r'))))
+	group_walk(~ create_rdump(as.data.frame(.), 'crownvolume', 'dbh_corr', file_name = file.path(fpdump, paste0('dump_volumescaling_', .y, '_1995.r'))))
 
 # ------------------------------------------------------------------------------ #
 # Create CSVs of minima, maxima, number of individuals, and normalization totals #
@@ -102,15 +99,13 @@ dat95 %>%
 
 years <- c(1985, 1990, 1995, 2000, 2005, 2010)
 
-##### EDIT BELOW DIS SPOT ######
-
 # Minima, maxima, and number of individuals 
 valall <- map2_dfr(alltreedat, years,
 				   ~ data.frame(fg = 'alltree', year = .y, xmin = min(.x$dbh_corr), n = nrow(.x)))
 
-valfg <- imap_dfr(fgdat, function(dat, fg_name) {
+valfg <- map2_dfr(fgdat, fgs, function(dat, fg_name) {
 	map2_dfr(dat, years,
-			 ~ data.frame(fg = fg_name, year = .y, xmin = min(dat$dbh_corr), n = nrow(dat)))
+			 ~ data.frame(fg = fg_name, year = .y, xmin = min(.x$dbh_corr), n = nrow(.x)))
 })
 
 min_n <- rbind(valall, valfg)
@@ -144,7 +139,7 @@ fgprod <- map2_dfr(alltreedat, years, function(x, y) {
 prodtotals <- rbind(alltreeprod, fgprod) %>%
   arrange(year, fg)
 
-write_csv(prodtotals, file = file.path(fpdump, 'production_total.csv'))
+write_csv(prodtotals, file.path(fpdump, 'production_total.csv'))
 
 # Total production for all trees that have light values
 alltreeprod <- sum(alltree_light_95$production)
@@ -156,7 +151,7 @@ fgprod <- alltree_light_95 %>%
 prodtotals <- data.frame(year = 1995, 
                          rbind(c(fg = 'alltree', production = alltreeprod), fgprod))
 
-write_csv(prodtotals, file = file.path(fpdump, 'production_total_lighttrees.csv'))
+write_csv(prodtotals, file.path(fpdump, 'production_total_lighttrees.csv'))
 
 # Light received totals
 alltreelightrec <- sum(dat95$light_received)
@@ -167,7 +162,7 @@ fglightrec <- dat95 %>%
 lightrectotals <- data.frame(year = 1995, 
                              rbind(c(fg = 'alltree', light_received = alltreelightrec), fglightrec))
 
-write_csv(lightrectotals, file = file.path(fpdump, 'lightrec_total.csv'))
+write_csv(lightrectotals, file.path(fpdump, 'lightrec_total.csv'))
 
 # Crown volume totals
 alltreecrownvol <- sum(dat95$crownvolume)
@@ -178,4 +173,4 @@ fgcrownvol <- dat95 %>%
 crownvolumetotals <- data.frame(year = 1995, 
                              rbind(c(fg = 'alltree', crownvolume = alltreecrownvol), fgcrownvol))
 
-write_csv(crownvolumetotals, file = file.path(fpdump, 'crownvol_total.csv'))
+write_csv(crownvolumetotals, file.path(fpdump, 'crownvol_total.csv'))
