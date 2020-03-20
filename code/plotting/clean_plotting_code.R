438788aa6ecdf12ead6584a52ebc8e21241963ae
John Grady <jgradym@gmail.com>
2020-03-20 10:09:25 -0500

    Update clean_plotting_code.R

--- code/plotting/clean_plotting_code.R
+++ code/plotting/clean_plotting_code.R
@@ -394,8 +394,7 @@ dev.off()
 # Model fit 2 = power law exp
 
 plot_prod2 <- function (year_to_plot = 1995, fg_names = c("fg1", "fg2", "fg3", 
-                                                          "fg4", "fg5", "all"), 
-                        model_fit = 1, x_limits, x_breaks = c(1, 3, 10, 30, 100, 300),
+                        "fg4", "fg5", "all"), model_fit = 1, x_limits, x_breaks = c(1, 3, 10, 30, 100, 300),
                         y_limits, y_labels, y_breaks, 
                         fill_names = c("#BFE046", "#267038", "#27408b", "#87Cefa", "gray87"), 
                         color_names = c("#BFE046",  "#267038", "#27408b", "#87Cefa", "gray"), 
@@ -410,47 +409,47 @@ plot_prod2 <- function (year_to_plot = 1995, fg_names = c("fg1", "fg2", "fg3",
   pos <- if (dodge_errorbar) 
     ggplot2::position_dodge(width = dodge_width)
   else "identity"
-  obsdat <- obsdat %>% dplyr::filter(fg %in% fg_names, year == 
-                                       year_to_plot, !is.na(mean), mean_n_individuals >= 20) %>% 
-    dplyr::group_by(bin_midpoint) %>% dplyr::mutate(width = error_bar_width * 
-                                                      dplyr::n()) %>% dplyr::ungroup()
+  obsdat <- obsdat %>% arrange(desc(fg)) %>% dplyr::filter(fg %in% fg_names, year == 
+     year_to_plot, !is.na(mean), mean_n_individuals >= 20) %>% 
+    dplyr::group_by(bin_midpoint)  %>% dplyr::mutate(width = error_bar_width * 
+     dplyr::n()) %>% dplyr::ungroup()
   obs_limits <- obsdat %>% dplyr::group_by(fg) %>% dplyr::summarize(min_obs = min(bin_midpoint), 
-                                                                    max_obs = max(bin_midpoint))
-  preddat <- preddat %>% dplyr::left_join(obs_limits) %>% dplyr::filter(prod_model %in% 
-                                                                          model_fit, fg %in% fg_names, year == year_to_plot) %>% 
+                max_obs = max(bin_midpoint))
+  preddat <- preddat %>% arrange(desc(fg)) %>% dplyr::left_join(obs_limits) %>% dplyr::filter(prod_model %in% 
+             model_fit, fg %in% fg_names, year == year_to_plot) %>% 
     dplyr::filter_at(dplyr::vars(dplyr::starts_with("q")), 
                      dplyr::all_vars(. > min(y_limits))) %>% dplyr::filter(dbh >= 
-                                                                             min_obs & dbh <= max_obs)
+           min_obs & dbh <= max_obs)
   p <- ggplot2::ggplot() + ggplot2::geom_ribbon(data = preddat, 
-                                                ggplot2::aes(x = dbh, ymin = q025, ymax = q975, group = fg, 
-                                                             fill = fg), alpha = 0.4) + ggplot2::geom_line(data = preddat, 
-                                                                                                           ggplot2::aes(x = dbh, y = q50, group = fg, color = fg))
+      ggplot2::aes(x = dbh, ymin = q025, ymax = q975, group = fg, 
+      fill = fg), alpha = 0.4) + ggplot2::geom_line(data = preddat, 
+      ggplot2::aes(x = dbh, y = q50, group = fg, color = fg))
   if (plot_errorbar) {
     p <- p + ggplot2::geom_errorbar(data = obsdat, ggplot2::aes_string(x = "bin_midpoint", 
-                                                                       ymin = error_min, ymax = error_max, group = "fg", 
-                                                                       color = "fg", width = "width"), position = pos, size = error_bar_thickness)
+         ymin = error_min, ymax = error_max, group = "fg", 
+         color = "fg", width = "width"), position = pos, size = error_bar_thickness)
   }
   p <- p + ggplot2::geom_line(data = preddat[preddat$fg == 
-                                               "fg5", ], ggplot2::aes(x = dbh, y = q50), color = "gray") + 
+       "fg5", ], ggplot2::aes(x = dbh, y = q50), color = "gray") + 
     ggplot2::geom_point(data = obsdat, ggplot2::aes_string(x = "bin_midpoint", 
-                                                           y = average, group = "fg", fill = "fg", arrange(rev(y_labels))), size = geom_size, 
-                        color = "black", shape = 21, position = pos) + 
-    ggplot2::scale_x_log10(name = x_name, limits = x_limits, breaks = x_breaks) + ggplot2::scale_y_log10(name = y_name, 
-                                                                                                                                                             limits = y_limits, breaks = y_breaks, labels = y_labels) + 
+     y = average, group = "fg", fill = "fg", size = geom_size, 
+    color = "black", shape = 21, position = pos) + ggplot2::scale_x_log10(name = x_name, 
+    limits = x_limits, breaks = x_breaks) + ggplot2::scale_y_log10(name = y_name, 
+    limits = y_limits, breaks = y_breaks, labels = y_labels) + 
     theme_no_x() + ggplot2::theme(rect = ggplot2::element_rect(fill = "transparent")) + 
     ggplot2::scale_color_manual(values = color_names) + ggplot2::scale_fill_manual(values = fill_names) + 
     theme_plant()
   if (plot_abline) {
     p <- p + ggplot2::geom_abline(intercept = abline_intercept, 
-                                  slope = abline_slope, color = "gray72", linetype = "dashed", 
-                                  size = 0.75)
+         slope = abline_slope, color = "gray72", linetype = "dashed", 
+         size = 0.75)
   }
   p
 }
 
 obs_indivprod <- obs_indivprod %>%
   filter(mean_n_individuals >= 20)
-p <- plot_prod(year_to_plot = 1995,
+p <- plot_prod2(year_to_plot = 1995,
           fg_names = c('fg1','fg2','fg3','fg4','fg5'),
           model_fit = PROD,
           x_limits = c(1, 230),
@@ -494,15 +493,15 @@ plot_totalprod2 <-function(year_to_plot = 1995, fg_names = c("fg1", "fg2", "fg3"
                             abline_intercept = 2) 
 {
   obsdat <- obsdat %>% dplyr::filter(fg %in% fg_names, year == 
-                                       year_to_plot, bin_count >= 20) %>% dplyr::filter(bin_value > 
-                                                                                         0)
+                                       year_to_plot, bin_count >= 20) %>% dplyr::filter(bin_value > 0) %>% arrange(desc(fg))
   obs_limits <- obsdat %>% dplyr::group_by(fg) %>% dplyr::summarize(min_obs = min(bin_midpoint), 
                                                                     max_obs = max(bin_midpoint))
   preddat <- preddat %>% dplyr::left_join(obs_limits) %>% dplyr::filter(dens_model %in% 
                                                                           model_fit_density, prod_model %in% model_fit_production, 
-                                                                        fg %in% fg_names, year == year_to_plot) %>% dplyr::filter_at(dplyr::vars(dplyr::starts_with("q")), 
-                                                                                                                                     dplyr::all_vars(. > min(y_limits))) %>% dplyr::filter(dbh >= 
-                                                                                                                                                                                             min_obs & dbh <= max_obs)
+                                                                        fg %in% fg_names, year == year_to_plot) %>% 
+    arrange(desc(fg)) %>% 
+    dplyr::filter_at(dplyr::vars(dplyr::starts_with("q")), 
+  dplyr::all_vars(. > min(y_limits))) %>% dplyr::filter(dbh >=  min_obs & dbh <= max_obs)
   p <- ggplot2::ggplot() + ggplot2::geom_ribbon(data = preddat, 
                                                 aes(x = dbh, ymin = q025, ymax = q975, group = fg, fill = fg), 
                                                 alpha = 0.4) + ggplot2::geom_line(data = preddat, aes(x = dbh, 
